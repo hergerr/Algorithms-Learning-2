eae0e592a90b99596fcec22be4921eed9beb91da
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2019-05-14 19:26:38 +0200

    Made Edge class

--- Edge.cpp
+++ Edge.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Edge.h"
+
+Edge::Edge(int src, int dest, int weight) {
+    this->source = src;
+    this->destination = dest;
+    this->weight = weight;
+}
+
+bool CompareWeight::operator()(Edge const &e1, Edge const &e2) {
+    return e1.weight > e2.weight;
+}
--- Edge.h
+++ Edge.h
@@ -6,22 +6,17 @@
 #define ALGORITHMS_LEARNING_2_EDGE_H
 
 
-struct Edge {
+class Edge {
+public:
     int source;
     int destination;
     int weight;
 
-    explicit Edge(int src = 0, int dest = 0, int weight = 0) {
-        this->source = src;
-        this->destination = dest;
-        this->weight = weight;
-    }
+    Edge(int src = 0, int dest = 0, int weight = 0);
 };
 
 struct CompareWeight {
-    bool operator()(Edge const& e1, Edge const& e2) {
-        return e1.weight > e2.weight;
-    }
+    bool operator()(Edge const& e1, Edge const& e2);
 };
 
 
