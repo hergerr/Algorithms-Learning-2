dc0617480bbef4a8fa46bbb8ea8177d603dc2032
Tymek <tymoteusz@frankiewicz.com.pl>
2019-05-15 22:54:25 +0200

    Found test bug

--- Test.cpp
+++ Test.cpp
@@ -14,160 +14,142 @@ void Test::run_tests() {
     for (int i = 0; i < 5; i++) {
         test_kruskal(sizes[i], 0.25);
 
-    }for (int i = 0; i < 5; i++) {
-        test_dijkstra(sizes[i], 0.25);
     }
 
     for (int i = 0; i < 5; i++) {
-        test_ford_bellman(sizes[i], 0.25);
+        test_dijkstra(sizes[i], 0.99);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        test_ford_bellman(sizes[i], 0.99);
     }
 }
 
 void Test::test_prim(int nodes, double density) {
     Adjacency_list al(false);
+    Adjacency_matrix am(false);
+
     vector<list<Edge>> g;
     chrono::duration<double> sum_list;
     chrono::duration<double> sum_matrix;
 
     for (int i = 0; i < tests; i++) {
         g = al.generate(nodes, density);
+        am.generate(g);
         auto beginning = std::chrono::steady_clock::now();
         al.prim();
         auto end = std::chrono::steady_clock::now();
         auto time = end - beginning;
         sum_list += time;
-    }
-    double average = (sum_list / tests).count();
 
-
-    cout << endl << "Lista Prim" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
-
-    Adjacency_matrix am(false);
-    for (int i = 0; i < tests; i++) {
-        am.generate(g);
-        auto beginning = std::chrono::steady_clock::now();
+        beginning = std::chrono::steady_clock::now();
         am.prim();
-        auto end = std::chrono::steady_clock::now();
-        auto time = end - beginning;
+        end = std::chrono::steady_clock::now();
+        time = end - beginning;
         sum_matrix += time;
     }
-    average = (sum_matrix / tests).count();
+    double average_list = (sum_list / tests).count();
+    double average_matrix = (sum_matrix / tests).count();
 
-    cout << endl << "Macierz Prim" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
 
-    cout << "\n=================================================================" << endl;
+    cout << endl << "Prim " << nodes << endl;
+    cout <<" Average List: " << average_list << endl;
+    cout <<" Average Matrix: " << average_matrix << endl;
+
 }
 
 void Test::test_kruskal(int nodes, double density) {
     Adjacency_list al(false);
+    Adjacency_matrix am(false);
+
     vector<list<Edge>> g;
     chrono::duration<double> sum_list;
     chrono::duration<double> sum_matrix;
 
     for (int i = 0; i < tests; i++) {
         g = al.generate(nodes, density);
+        am.generate(g);
         auto beginning = std::chrono::steady_clock::now();
         al.kruskal();
         auto end = std::chrono::steady_clock::now();
         auto time = end - beginning;
         sum_list += time;
-    }
-    double average = (sum_list / tests).count();
 
-
-    cout << endl << "Lista Kruskal" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
-
-    Adjacency_matrix am(false);
-    for (int i = 0; i < tests; i++) {
-        am.generate(g);
-        auto beginning = std::chrono::steady_clock::now();
+        beginning = std::chrono::steady_clock::now();
         am.kruskal();
-        auto end = std::chrono::steady_clock::now();
-        auto time = end - beginning;
+        end = std::chrono::steady_clock::now();
+        time = end - beginning;
         sum_matrix += time;
     }
-    average = (sum_matrix / tests).count();
+    double average_list = (sum_list / tests).count();
+    double average_matrix = (sum_matrix / tests).count();
 
-    cout << endl << "Macierz Kruskal" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
 
-    cout << "\n=================================================================" << endl;
+    cout << endl << "Kruskal " << nodes << endl;
+    cout <<" Average List: " << average_list << endl;
+    cout <<" Average Matrix: " << average_matrix << endl;
 
 }
 
 void Test::test_dijkstra(int nodes, double density) {
-    Adjacency_list al(false);
+    Adjacency_list al(true);
+    Adjacency_matrix am(true);
     vector<list<Edge>> g;
     chrono::duration<double> sum_list;
     chrono::duration<double> sum_matrix;
 
     for (int i = 0; i < tests; i++) {
         g = al.generate(nodes, density);
+        am.generate(g);
         auto beginning = std::chrono::steady_clock::now();
         al.dijkstra();
         auto end = std::chrono::steady_clock::now();
         auto time = end - beginning;
         sum_list += time;
-    }
-    double average = (sum_list / tests).count();
 
-
-    cout << endl << "Lista Dijkstra" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
-
-    Adjacency_matrix am(false);
-    for (int i = 0; i < tests; i++) {
-        am.generate(g);
-        auto beginning = std::chrono::steady_clock::now();
+        beginning = std::chrono::steady_clock::now();
         am.dijkstra();
-        auto end = std::chrono::steady_clock::now();
-        auto time = end - beginning;
+        end = std::chrono::steady_clock::now();
+        time = end - beginning;
         sum_matrix += time;
     }
-    average = (sum_matrix / tests).count();
+    double average_list = (sum_list / tests).count();
+    double average_matrix = (sum_matrix / tests).count();
 
-    cout << endl << "Macierz Dijkstra" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
 
-    cout << "\n=================================================================" << endl;
+    cout << endl << "Dijkstra " << nodes << endl;
+    cout <<" Average List: " << average_list << endl;
+    cout <<" Average Matrix: " << average_matrix << endl;
+
 }
 
 void Test::test_ford_bellman(int nodes, double density) {
-    Adjacency_list al(false);
+    Adjacency_list al(true);
+    Adjacency_matrix am(true);
     vector<list<Edge>> g;
     chrono::duration<double> sum_list;
     chrono::duration<double> sum_matrix;
 
     for (int i = 0; i < tests; i++) {
         g = al.generate(nodes, density);
+        am.generate(g);
         auto beginning = std::chrono::steady_clock::now();
         al.ford_bellman();
         auto end = std::chrono::steady_clock::now();
         auto time = end - beginning;
         sum_list += time;
-    }
-    double average = (sum_list / tests).count();
 
-
-    cout << endl << "Lista Ford-Bellman" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
-
-    Adjacency_matrix am(false);
-    for (int i = 0; i < tests; i++) {
-        am.generate(g);
-        auto beginning = std::chrono::steady_clock::now();
+        beginning = std::chrono::steady_clock::now();
         am.ford_bellman();
-        auto end = std::chrono::steady_clock::now();
-        auto time = end - beginning;
+        end = std::chrono::steady_clock::now();
+        time = end - beginning;
         sum_matrix += time;
     }
-    average = (sum_matrix / tests).count();
+    double average_list = (sum_list / tests).count();
+    double average_matrix = (sum_list / tests).count();
 
-    cout << endl << "Macierz Ford-Bellman" << endl;
-    cout << "Nodes: " << nodes << " Density: " << density << " Average: " << average << endl;
 
-    cout << "\n=================================================================" << endl;
+    cout << endl << "Ford-Bellman " << nodes << endl;
+    cout <<" Average List: " << average_list << endl;
+    cout <<" Average Matrix: " << average_matrix << endl;
 }
